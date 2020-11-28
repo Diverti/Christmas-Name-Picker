3ac01dc8d8cea2b7b9b12ca9aec4f0701b3e2189
Diverti <diverti1@hotmail.hu>
2020-11-28 10:01:50 +0100

    update

--- pick.php
+++ pick.php
@@ -1,5 +1,9 @@
 <?php
 $names = file("names.txt", FILE_IGNORE_NEW_LINES);
+if(sizeof($names) != $_GET['num']) {
+    echo "RELOAD";
+    die();
+}
 $index = substr($_GET['q'],-1);
 $name= $names[$index];
 file_put_contents("done.txt", $name."\n", FILE_APPEND);
